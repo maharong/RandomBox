39aaccc7a095e64df273603f40100255359cb18a
maharong <kija4545@naver.com>
2025-07-25 16:36:16 +0900

    Initial commit

---
+++ src/main/java/com/github/maharong/randombox/RandomBox.java
@@ -0,0 +1,17 @@
+package com.github.maharong.randombox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RandomBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
